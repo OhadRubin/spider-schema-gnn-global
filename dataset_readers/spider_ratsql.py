495d842e3572606aecf491c94ed44f99ff1beabf
Unknown <iohadrubin@gmail.com>
2020-07-17 09:23:49 +0300

    .

--- dataset_readers/spider_ratsql.py
+++ dataset_readers/spider_ratsql.py
@@ -27,9 +27,37 @@ import torch
 from tqdm import tqdm
 
 
+
+import json
+import logging
+import os
+from typing import List, Dict
+import codecs
+
+import dill
+from allennlp.common.checks import ConfigurationError
+from allennlp.data import DatasetReader, Tokenizer, TokenIndexer, Field, Instance
+from allennlp.data.fields import TextField, ListField, IndexField, MetadataField, ArrayField
+from allennlp.data.token_indexers import SingleIdTokenIndexer
+# from allennlp.data.tokenizers import WordTokenizer
+from allennlp.data.tokenizers.spacy_tokenizer import SpacyTokenizer
+from overrides import overrides
+from spacy.symbols import ORTH, LEMMA
+
+from dataset_readers.dataset_util.spider_utils import fix_number_value, disambiguate_items
+from dataset_readers.fields.spider_knowledge_graph_field import SpiderKnowledgeGraphField
+from dataset_readers.fields.production_rule_field import ProductionRuleField
+from semparse.contexts.spider_db_context import SpiderDBContext
+from semparse.worlds.spider_world import SpiderWorld
+import pickle
+import jsonpickle
+from time import time
+import pathlib
+
+logger = logging.getLogger(__name__)
+
 STOPWORDS = set(nltk.corpus.stopwords.words('english'))
 PUNKS = set(a for a in string.punctuation)
-
 
 def clamp(value, abs_max):
     value = max(-abs_max, value)
@@ -110,15 +138,37 @@ class PreprocessedSchema:
     primary_keys = attr.ib(factory=list)
 
 
-class SpiderEncoderV2Preproc:
-    def __init__(
-            self,
-            include_table_name_in_column=True,
-            fix_issue_16_primary_keys=False,
-            qq_max_dist=2,
-            cc_max_dist=2,
-            tt_max_dist=2):
 
+
+@DatasetReader.register("spider_ratsql")
+class SpiderRatsqlDatasetReader(DatasetReader):
+    def __init__(self,
+                lazy: bool = False,
+                question_token_indexers: Dict[str, TokenIndexer] = None,
+                keep_if_unparsable: bool = True,
+                tables_file: str = None,
+                dataset_path: str = 'dataset/database',
+                cache_directory: str = "cache/train",
+                include_table_name_in_column=True,
+                fix_issue_16_primary_keys=False,
+                qq_max_dist=2,
+                cc_max_dist=2,
+                tt_max_dist=2,
+                max_instances = None):
+        super().__init__(lazy=lazy,cache_directory=cache_directory,max_instances=max_instances)
+
+        # default spacy tokenizer splits the common token 'id' to ['i', 'd'], we here write a manual fix for that
+        spacy_tokenizer = SpacyTokenizer(pos_tags=True)
+        spacy_tokenizer.spacy.tokenizer.add_special_case(u'id', [{ORTH: u'id', LEMMA: u'id'}])
+        self._tokenizer = spacy_tokenizer
+
+        self._utterance_token_indexers = question_token_indexers or {'tokens': SingleIdTokenIndexer()}
+        self._keep_if_unparsable = keep_if_unparsable
+
+        self._tables_file = tables_file
+        self._dataset_path = dataset_path
+
+        #ratsql
         self.include_table_name_in_column = include_table_name_in_column
         self.fix_issue_16_primary_keys = fix_issue_16_primary_keys
         self.texts = collections.defaultdict(list)
@@ -150,6 +200,160 @@ class SpiderEncoderV2Preproc:
                     "qcNUMBER","cqNUMBER","qcTIME","cqTIME","qcCELLMATCH","cqCELLMATCH",]
         for rel in rel_names:
             add_relation(rel)
+
+        self.schemas=None
+        self.eval_foreign_key_maps=None
+        self.schemas, self.eval_foreign_key_maps = self.load_tables([self._tables_file])
+
+        for db_id, schema in self.schemas.items():
+            sqlite_path = Path(self._dataset_path) / db_id / f"{db_id}.sqlite"
+            source: sqlite3.Connection
+            with sqlite3.connect(sqlite_path) as source:
+                dest = sqlite3.connect(':memory:')
+                dest.row_factory = sqlite3.Row
+                source.backup(dest)
+            schema.connection = dest
+        # super().__init__(lazy=lazy,max_instances=max_instances)
+
+
+    
+
+
+    @overrides
+    def _read(self, file_path: str):
+        if file_path.endswith('.json'):
+            yield from self._read_examples_file(file_path)
+        else:
+            raise ConfigurationError(f"Don't know how to read filetype of {file_path}")
+
+    def _read_examples_file(self, file_path: str):
+        cache_dir = os.path.join('cache', file_path.split("/")[-1])
+
+
+        cnt = 0
+        with open(file_path, "r") as data_file:
+            
+            json_obj = json.load(data_file)
+            for total_cnt, ex in enumerate(json_obj):
+
+                query_tokens = None
+                if 'query_toks' in ex:
+
+                    ex = fix_number_value(ex)
+
+                    try:
+                        query_tokens = disambiguate_items(ex['db_id'], ex['query_toks_no_value'],
+                                                                self._tables_file, allow_aliases=False)
+                    except Exception as e:
+                        # there are two examples in the train set that are wrongly formatted, skip them
+                        print(f"error with {ex['query']}")
+                        print(e)
+
+                ins = self.text_to_instance(
+                    utterance=ex['question'],
+                    db_id=ex['db_id'],
+                    sql=query_tokens,orig=ex)
+                ins = self.process_instance(ins, total_cnt)
+
+                if ins is not None:
+                    yield ins
+
+    def text_to_instance(self,
+                         utterance: str,
+                         db_id: str,
+                         sql: List[str] = None,
+                         orig=None):
+        # print("bye")
+        fields: Dict[str, Field] = {}
+        item = SpiderItem(
+            text=orig['question_toks'],
+            code=orig['sql'],
+            schema=self.schemas[db_id],
+            orig=orig,
+            orig_schema=self.schemas[db_id].orig)
+
+        desc = self.preprocess_item(item,"train")
+        q = desc['question']
+        t = [x[0] for x in  desc['tables']]
+        c = ["_".join(x) for x in  desc['columns']]
+        enc = q+c+t
+        # print(enc)
+        relation = self.compute_relations(desc,len(enc),len(q),len(c),range(len(c)+1),range(len(t)+1))
+        #old code
+        relation_field = ArrayField(relation,padding_value=-1,dtype=np.int32)
+        fields['relation'] = relation_field
+
+        db_context = SpiderDBContext(db_id, utterance, tokenizer=self._tokenizer,
+                                     tables_file=self._tables_file, dataset_path=self._dataset_path)
+        table_field = SpiderKnowledgeGraphField(db_context.knowledge_graph,
+                                                db_context.tokenized_utterance,
+                                                self._utterance_token_indexers,
+                                                entity_tokens=db_context.entity_tokens,
+                                                include_in_vocab=False,
+                                                max_table_tokens=None)
+
+        world = SpiderWorld(db_context, query=sql)
+        fields["utterance"] = TextField(db_context.tokenized_utterance, self._utterance_token_indexers)
+
+        action_sequence, all_actions = world.get_action_sequence_and_all_actions()
+
+        if action_sequence is None and self._keep_if_unparsable:
+            # print("Parse error")
+            action_sequence = []
+        elif action_sequence is None:
+            return None
+
+        index_fields: List[Field] = []
+        production_rule_fields: List[Field] = []
+
+        for production_rule in all_actions:
+            nonterminal, rhs = production_rule.split(' -> ')
+            production_rule = ' '.join(production_rule.split(' '))
+            field = ProductionRuleField(production_rule,
+                                        world.is_global_rule(rhs),
+                                        nonterminal=nonterminal)
+            production_rule_fields.append(field)
+
+        valid_actions_field = ListField(production_rule_fields)
+        fields["valid_actions"] = valid_actions_field
+
+        action_map = {action.rule: i  # type: ignore
+                      for i, action in enumerate(valid_actions_field.field_list)}
+
+        for production_rule in action_sequence:
+            index_fields.append(IndexField(action_map[production_rule], valid_actions_field))
+        if not action_sequence:
+            index_fields = [IndexField(-1, valid_actions_field)]
+
+        action_sequence_field = ListField(index_fields)
+        fields["action_sequence"] = action_sequence_field
+        fields["world"] = MetadataField(world)
+        fields["schema"] = table_field
+        for key,value  in table_field.__dict__.items():
+            pickled =  dill.dumps(value)
+            res = jsonpickle.encode(pickled)
+        ins = Instance(fields)
+
+        return  ins
+
+    def process_instance(self, instance: Instance, index: int):
+        return instance
+
+
+    @overrides
+    def _instances_from_cache_file(self, cache_filename: str):
+        with open(cache_filename, "rb") as cache_file:
+            yield from dill.load(cache_file)
+            # for line in cache_file:
+                # yield self.deserialize_instance(line.strip())
+
+    @overrides
+    def _instances_to_cache_file(self, cache_filename, instances) -> None:
+        with open(cache_filename, "wb") as cache:
+            dill.dump(instances,cache)
+            # for instance in Tqdm.tqdm(instances):
+                # cache.write(self.serialize_instance(instance) + "\n")
+
 
     def compute_relations(self, desc, enc_length, q_enc_length, c_enc_length, c_boundaries, t_boundaries):
         sc_link = desc.get('sc_link', {'q_col_match': {}, 'q_tab_match': {}})
